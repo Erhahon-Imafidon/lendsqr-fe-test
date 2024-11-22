1e16032b8e945a6d061b1e6f25250c804a2c7a64
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-22 03:43:13 +0100

    installed svgr dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        svgr({
+            // Set it to `true` to export React component as default.
+            // Notice that it will override the default behavior of Vite.
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            exportAsDefault: false,
+
+            //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include. By default all svg files will be included.
+            include: '**/*.svg',
+
+            //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+            exclude: '',
+        }),
+    ],
 });
