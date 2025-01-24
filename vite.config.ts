dcdeb44980f08c4124146bce07d4b403336bf8a0
Erhahon Imafidon <mafiosco4@gmail.com>
2025-01-24 12:07:37 +0100

    installed vite-plugin-purgecss to purge unused css in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import PurgeCSS from 'vite-plugin-purgecss';
+import type { Plugin } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-
+        PurgeCSS({
+            content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+        }) as unknown as Plugin,
         svgr({
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-expect-error
