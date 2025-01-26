278accbb46f81807eda897111240967cc172b52b
Erhahon Imafidon <mafiosco4@gmail.com>
2025-01-26 10:23:55 +0100

    created the headed component directory and file

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,18 +1,17 @@
 import './login.scss';
 import Logo from '../../assets/images/logo.png';
+import SignIn from '../../assets/images/sign-in.png';
 
 const Login = () => {
     return (
         <div className="login">
             <section className="image bg-secondary">
-                <img src={Logo} alt="logo" />
+                <img src={Logo} alt="logo" className="logoImg" />
+
+                <div></div>
             </section>
 
-            <section className="welcome bg-white">
-                <header>
-                    <h1>Welcome</h1>
-                </header>
-            </section>
+            <section className="welcome bg-white"></section>
         </div>
     );
 };
